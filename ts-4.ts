7a13d5cfc5361ef9a881e55c9728d67607d43c06
jianqing <jianqing@luojilab.com>
2020-03-11 23:23:53 +0800

    泛型

---
+++ ts-4.ts
@@ -0,0 +1,61 @@
+function identity<T>(arg: T):T {
+  return arg
+}
+// let output = identity<string>('myString')
+
+let output = identity('myString')
+
+function loggingIndentity<T>(arg:T[]): T[]{
+  console.log(arg.length)
+  return arg;
+}
+interface GericIdentityFn<T> {
+  (arg: T): T
+}
+
+let myIdentity: GericIdentityFn<number> = identity
+
+class GenericNumber<T> {
+  zeroValue: T
+  add: (x: T, y: T) => T
+}
+
+let myGenricNumber = new GenericNumber<number>()
+myGenricNumber.zeroValue = 0
+myGenricNumber.add = function(x, y){
+  return x + y
+}
+
+interface Lengthwise {
+  length: number
+}
+
+function logginIn <T extends Lengthwise>(arg: T): T{
+  console.log(arg.length)
+  return arg
+}
+
+function getProperty <T, K extends keyof T>(obj: T, key: K) {
+  return obj[key]
+}
+
+let x = {a: 1, b: 2, c: 3, d: 4}
+getProperty(x, 'a')
+// getProperty(x, 'u') // 报错
+
+// 工厂函数
+function create<T>(c: {new():T}): T {
+  return new c()
+}
+
+class BeeKeeper {
+  hasMask: boolean
+}
+
+class LionKeeper {
+  nametag: string
+}
+class Animal {
+  numLengs: number
+}
+
